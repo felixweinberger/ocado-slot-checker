8b952690e2d1cc9045feb561025577096a507ef3
Felix Weinberger <felix.weinberger@samsara.com>
2020-06-13 15:36:48 +0100

    ocado: refactor and add macOS desktop notifications

--- ocado.js
+++ ocado.js
@@ -1,9 +1,49 @@
 const puppeteer = require('puppeteer');
+const cron = require('node-cron');
+const notifier = require('node-notifier');
 
-(async () => {
+// cron.schedule('*/1 * * * *', checkSlotsAndNotify)
+checkSlotsAndNotify();
+
+async function checkSlotsAndNotify() {
+  const {
+    hasSlotsAvailable,
+    isDeliverySlotPage
+  } = await checkAvailableSlots();
+
+  const now = new Date();
+  const dateString = now.toLocaleDateString('en-GB', {day: 'numeric', month:'short', weekday: 'short'});
+  const timeString = now.toLocaleTimeString('en-GB');
+  const dateTimeString = '[' + dateString + ' ' + timeString + ']';
+
+  if (!isDeliverySlotPage) {
+    console.log(dateTimeString + " 🤬 You were logged out of Ocado! Run chrome in GUI mode, login, and restart the Ocado checker.")
+    notifier.notify({
+      title: "Ocado checker",
+      sound: true,
+      message: "🤬 You were logged out of Ocado!"
+    });
+    return;
+  }
+
+  if (!hasSlotsAvailable) {
+    console.log(dateTimeString + " 😱 Slots available! Check https://www.ocado.com/webshop/getAddressesForDelivery.do right now!");
+    notifier.notify({
+      title: "Ocado checker",
+      sound: true,
+      open: "https://www.ocado.com/webshop/getAddressesForDelivery.do",
+      message: "😱 Slots available!"
+    });
+  } else {
+    console.log(dateTimeString + " 😭 No slots available.");
+  }
+  return;
+}
+
+async function checkAvailableSlots() {
   // Configure browser
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     args: [
       "--window-size=1920,1200",
@@ -16,14 +56,16 @@ const puppeteer = require('puppeteer');
   await page.goto('https://www.ocado.com/webshop/getAddressesForDelivery.do', {waitUntil: 'networkidle2'});
   await page.setViewport({ width: 1920, height: 1200});
 
+  // Check if on correct page (not login page)
+  const html = await page.content();
+  const isDeliverySlotPage = /choose a slot/gi.test(html);
+
   // Check if html contains
-  const html = await page.content()
-  const re = /sorry\, no slots available for the selected days./gi
-  const hasSlots = !re.test(html)
+  const hasSlotsAvailable = !/sorry\, no slots available for the selected days./gi.test(html) && isDeliverySlotPage;
 
-  // Print out result
-  console.log("hasSlots: ", hasSlots)
-
-  // Clean up
-  await browser.close()
-})();
+  // Print out results
+  return {
+    isDeliverySlotPage,
+    hasSlotsAvailable
+  };
+};
